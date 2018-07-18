49d35d04915b622c6b655cf188c5b1c002cbdc2f
madhurima85 <madhu.chowdhury85@gmail.com>
2018-07-18 20:06:19 +0200

    Signed-off-by: madhurima85 <madhu.chowdhury85@gmail.com>

--- backbase.qa/src/test/java/factory/DataProviderFactory.java
+++ backbase.qa/src/test/java/factory/DataProviderFactory.java
@@ -2,20 +2,22 @@ package factory;
 
 import dataProvider.ExcelDataProvider;
 
-public class DataProviderFactory 
-{
+public class DataProviderFactory {
 
-	// Method to create object of ExcelDataProvider class
-	public static ExcelDataProvider getExcel()
-	{
-		ExcelDataProvider excel= new ExcelDataProvider("./TestData/BBTestData.xlsx");
+	// Method to create object of ExcelDataProvider class and will return excel
+	public static ExcelDataProvider getExcel() {
+		
+		// Create an object of ExcelDataProvider class
+		ExcelDataProvider excel = new ExcelDataProvider("./TestData/BBTestData.xlsx");
 		return excel;
 	}
-	
-	// Method to create another object of ExcelDataProvider class
-	public static ExcelDataProvider getanotherExcel()
-	{
-		ExcelDataProvider excel= new ExcelDataProvider("./TestData/BBTestDataProvider.xlsx");
+
+	// Method to create another object of ExcelDataProvider class and will return
+	// excel
+	public static ExcelDataProvider getanotherExcel() {
+		
+		// Create an object of ExcelDataProvider class
+		ExcelDataProvider excel = new ExcelDataProvider("./TestData/BBTestDataProvider.xlsx");
 		return excel;
 	}
 }
