ddde3950e29c8df887776013b90d5bb0b64f6f4c
madhurima85 <madhu.chowdhury85@gmail.com>
2018-07-18 20:09:43 +0200

    Signed-off-by: madhurima85 <madhu.chowdhury85@gmail.com>

--- backbase.qa/src/test/java/library/Utility.java
+++ backbase.qa/src/test/java/library/Utility.java
@@ -16,154 +16,179 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Utility 
-{
-	
+public class Utility {
+
 	static WebDriverWait wait;
-	
-	//Method to wait for an element to be visible or clickable and return the webelement
-	public static WebElement waitforElementVisible(WebDriver driver,WebElement element)
-	{
-		//create an object of webdriver wait
-		wait= new WebDriverWait(driver, 30);
-		
-		//wait until webelement is visible
+
+	// Method to wait for an element to be visible or clickable and return the
+	// webelement
+	public static WebElement waitforElementVisible(WebDriver driver, WebElement element) {
+
+		// create an object of webdriver wait
+		wait = new WebDriverWait(driver, 30);
+
+		// wait until webelement is visible
 		wait.until(ExpectedConditions.visibilityOf(element));
-		
-		//wait until webelement is clickable
+
+		// wait until webelement is clickable
 		wait.until(ExpectedConditions.elementToBeClickable(element));
-		
-		//return webelement
+
+		// return webelement
 		return element;
 	}
 
-	//Method to halt the program for given time
-	public static void  sleep(int time)
-	{
-		//halt the program
+	// Method to halt the program for given time
+	public static void sleep(int time) {
+
+		// halt the program for given time
 		try {
-			Thread.sleep(time*1000);
-		} 
-		
-		//print the exception
+			Thread.sleep(time * 1000);
+		}
+
+		// print the exception
 		catch (Exception e) {
-			
+
 			e.printStackTrace();
 		}
 	}
-	
-	public enum DropDownType
-	{
+
+	// Enum consisting set of values
+	public enum DropDownType {
 		BY_INDEX, BY_VALUE, BY_VISIBLE_TEXT;
 	}
-	
-	//Method to select value from dropdown by index/value/visible text
-	
-	public static void selectBydropdown(WebDriver driver, WebElement element, DropDownType type, Object value)
-	{
+
+	// Method to select value from dropdown by index/value/visible text
+	public static void selectBydropdown(WebDriver driver, WebElement element, DropDownType type, Object value) {
+
+		// wait for 30 seconds until the webelement is visible
 		new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOf(element));
-		Select select= new Select(element);
-		
-		switch(type)
-		{
+
+		// Create a select class object on webelement
+		Select select = new Select(element);
+
+		switch (type) {
 		case BY_INDEX:
-			int index = (Integer)value;
+			int index = (Integer) value;
+
+			// Select dropdown value by index
 			select.selectByIndex(index);
+
 			break;
 		case BY_VALUE:
-			String val = (String)value;
+			String val = (String) value;
+
+			// Select dropdown data by value
 			select.selectByValue(val);
+
 			break;
 		case BY_VISIBLE_TEXT:
-			String visible_text=(String)value;
+			String visible_text = (String) value;
+
+			// Select dropdown value by visible text
 			select.selectByVisibleText(visible_text);
 			break;
 		}
 	}
-	
-	//Method to generate random string of given length
-	public static String generateRandomString(int length) 
-	{
-		String alphabet= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		int N= alphabet.length();
-		Random r= new Random();
-		
-		String randomstr="";
-		for (int i=0;i<length; i++)
-		{
-			randomstr= randomstr+alphabet.charAt(r.nextInt(N));
+
+	// Method to generate random string of given length
+	public static String generateRandomString(int length) {
+
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		// length of alphabet
+		int N = alphabet.length();
+
+		// Create object of Random class
+		Random r = new Random();
+
+		String randomstr = "";
+
+		// Initiate a for loop
+		for (int i = 0; i < length; i++) {
+
+			// Create a random alphabet string of given length
+			randomstr = randomstr + alphabet.charAt(r.nextInt(N));
 		}
+
+		// Return the random string
 		return randomstr;
 	}
 
-	//Method to wait for an element to be visible and return true or false
-	public static boolean verifyElement(WebDriver driver,WebElement element, int second)
-	{
+	// Method to wait for an element to be visible and return true or false
+	public static boolean verifyElement(WebDriver driver, WebElement element, int second) {
+
 		boolean isFound = true;
-		try{
+		try {
+			// wait for 30 seconds until the webelement is visible
 			new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(element));
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
+
 			isFound = false;
 		}
+
+		// return true if webelement is found or else return false
 		return isFound;
 	}
-	
-	public static String dateFormat(String dateStr) 
-	{
-			
-			
-			//String dateStr = "21/20/2011";  //2017-05-10
-			//String dateStr = "2017-05-10";
-			//Date format being filled while update of Computer
-		   			try {
-				DateFormat srcDf = new SimpleDateFormat("yyyy-MM-dd");
-				
-				// parse the date string into Date object
-				java.util.Date date = srcDf.parse(dateStr);
-				
-				DateFormat destDf = new SimpleDateFormat("dd MMM yyyy");
-				 
-				// format the date into another format
-				dateStr = destDf.format(date);
-				
-		   			} 
-			catch (ParseException e) {
-				e.printStackTrace();
-			}
-			return dateStr;
-		
-			}
-	
-	public static String getCurrentDate_Time() 
-	{
+
+	// Method to convert a date format to another format and return the date
+	public static String dateFormat(String dateStr) {
+
+		// Date format being filled while update of Computer
+		try {
+			DateFormat srcDf = new SimpleDateFormat("yyyy-MM-dd");
+
+			// Parse the date string into Date object
+			java.util.Date date = srcDf.parse(dateStr);
+
+			DateFormat destDf = new SimpleDateFormat("dd MMM yyyy");
+
+			// Format the date into another format
+			dateStr = destDf.format(date);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return dateStr;
+
+	}
+
+	// Method to generate current date and time in the desired format
+	// return the dateandtime
+	public static String getCurrentDate_Time() {
+
+		// Create a Date object
 		Date data = new Date();
 
+		// Create an object of SimpleDateFormat with the given format
 		SimpleDateFormat dateFormater = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
 
+		// Return the date in the given format
 		return dateFormater.format(data);
 	}
 
+	// Method to capture screenshot and return the file destination
 	public static String captureScreenshot(WebDriver driver) {
 
+		// Convert web driver object to TakeScreenshot- typecasting
 		TakesScreenshot ts = (TakesScreenshot) driver;
-		
+
+		// Take screenshot and store as a file format
 		File src = ts.getScreenshotAs(OutputType.FILE);
 
-		String destination = System.getProperty("user.dir")+"\\Screenshots\\Backbase_" + getCurrentDate_Time() + ".png";
+		// File destination
+		String destination = System.getProperty("user.dir") + "\\Screenshots\\Backbase_" + getCurrentDate_Time()
+				+ ".png";
 		try {
-			
+
+			// now copy the screenshot to desired location using copyFile
 			FileUtils.copyFile(src, new File(destination));
-			
-			} catch (Exception e) {
+
+		} catch (Exception e) {
 			System.out.println("Unable to capture screenshots " + e.getMessage());
 		}
-
+		// return file destination
 		return destination;
 	}
-	
 
-
-		
-	
 }
